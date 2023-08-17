2d62af65068d2f309cd3b311eb3fc8381282c32f
Joshua Mclean <joshmcleana@gmail.com>
2023-08-17 11:38:24 -0700

    Initial Commit

---
+++ Folio_IO/lib/models/cover_letter.rb
@@ -0,0 +1,5 @@
+class CoverLetter
+    attr_accessor :company_name, :position_title, :personal_info, :content
+    
+    #Implement methods to set and retrieve other details.
+end
---
+++ Folio_IO/lib/models/resume.rb
@@ -0,0 +1,5 @@
+class Resume
+    attr_accessor :full_name, :role_title, :email, :phone_number, :location, :portfolio_links
+
+    #Implement methods to set and retrieve other details.
+end
